6f64d2bd48e72e13bc218098753cad91f22113cb
mdanikibun <md.anikibun@gmail.com>
2025-02-28 15:32:09 +0700

    feat(contract-hash): update new hash for contract transaction

--- Context/constants.js
+++ Context/constants.js
@@ -2,5 +2,5 @@
 // https://sepolia.etherscan.io/ > find: 0x9853A64c78a0B6A53Cbb52ad2de46121e8f2D3Ca => check transaction
 import nftMarketplace from "./NFTMarketplace.json";
 
-export const NFTMarketplaceAddress = "0xD93FC7e58a45C6EAb2a87b6AE33A258129abdd89";
+export const NFTMarketplaceAddress = "0x9853A64c78a0B6A53Cbb52ad2de46121e8f2D3Ca";
 export const NFTMarketplaceABI = nftMarketplace.abi;
