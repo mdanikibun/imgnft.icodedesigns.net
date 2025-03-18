f94c64f8830f587c87048278f5e70697cdcf099e
mdanikibun <md.anikibun@gmail.com>
2025-03-18 21:09:52 +0700

    feat(history): add transaction history on Discover

--- components/NavBar/Discover.jsx
+++ components/NavBar/Discover.jsx
@@ -19,15 +19,26 @@ const Discover = () => {
         {
             name: "Kết nối ví điện tử",
             link: "connectWallet"
+        },
+        {
+            name: "Lịch sử giao dịch",
+            link: "https://sepolia.etherscan.io/address/0xaA56808a1A7C405E574D8320E64377Be0c46A859#nfttransfers"
         }
     ];
     return (
         <div>
             {discover.map((el, i) => (
                 <div key={i + 1} className={Style.discover}>
-                    <Link href={{pathname: `${el.link}`}}>
-                        {el.name}
-                    </Link>
+                    {el.name === "Lịch sử giao dịch" && (
+                        <Link href={{pathname: `${el.link}`}} target="_blank">
+                            {el.name}
+                        </Link>
+                    )}
+                    {el.name !== "Lịch sử giao dịch" && (
+                        <Link href={{pathname: `${el.link}`}}>
+                            {el.name}
+                        </Link>
+                    )}
                 </div>
             ))}
         </div>
