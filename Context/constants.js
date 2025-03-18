13d23c79356d87c7322904209aa63a5ef6892a6d
mdanikibun <md.anikibun@gmail.com>
2025-03-18 09:46:39 +0700

    feat(contract-hash): generate new contract-hash for live

--- Context/constants.js
+++ Context/constants.js
@@ -1,8 +1,8 @@
-//LIVE-CONTRACT: 0x1b699B0ae32C9394B636464F2434966289AbAc43
-// https://sepolia.etherscan.io/ > find: 0x1b699B0ae32C9394B636464F2434966289AbAc43 => check transaction
+//LIVE-CONTRACT: 0xaA56808a1A7C405E574D8320E64377Be0c46A859
+// https://sepolia.etherscan.io/ > find: 0xaA56808a1A7C405E574D8320E64377Be0c46A859 => check transaction
 import nftMarketplace from "./NFTMarketplace.json";
 
-export const NFTMarketplaceAddress = "0x1b699B0ae32C9394B636464F2434966289AbAc43"; // live
+export const NFTMarketplaceAddress = "0xaA56808a1A7C405E574D8320E64377Be0c46A859"; // live
 
 // TODO: comment lại code khi up-git để tránh xung đột với LIVE
 // export const NFTMarketplaceAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // localhost
